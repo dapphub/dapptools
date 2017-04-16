9bd41e42fff2f841fe4ee57b515d9cb2cd28863b
Daniel Brockman <daniel@brockman.se>
2017-04-16 02:58:42 -0400

    default.nix: formatting

--- default.nix
+++ default.nix
@@ -1,19 +1,15 @@
 let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
-in rec {
+in {
   seth = stdenv.mkDerivation rec {
-  name = "seth";
-  src = ./.;
-  buildInputs = [pkgs.makeWrapper];
-  makeFlags = ["prefix=$(out)"];
-  buildPhase = "true";
-  postInstall =
-    let
-      path = pkgs.lib.makeBinPath [
-        pkgs.bc pkgs.curl pkgs.ethabi pkgs.git pkgs.jshon pkgs.solc
-      ];
-    in
-      ''wrapProgram "$out/bin/seth" --prefix PATH : "${path}"'';
+    name = "seth";
+    src = ./.;
+    buildInputs = [pkgs.makeWrapper];
+    makeFlags = ["prefix=$(out)"];
+    buildPhase = "true";
+    postInstall = let path = pkgs.lib.makeBinPath [
+      pkgs.bc pkgs.curl pkgs.ethabi pkgs.git pkgs.jshon pkgs.solc
+    ]; in ''wrapProgram "$out/bin/seth" --prefix PATH : "${path}"'';
   };
 }
