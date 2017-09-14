617d68fea747f3c237cdd330dd407a046cb2af26
Mikael Brockman <mikael@brockman.se>
2017-09-14 14:03:44 +0300

    fix shell.nix for restless-git

--- shell.nix
+++ shell.nix
@@ -9,7 +9,12 @@ let
 #    };
 #  };
 
-  restless-git = import /home/mbrock/projects/restless-git;
+  restless-git = import (pkgs.fetchFromGitHub {
+    owner = "lessrest";
+    repo = "restless-git";
+    rev = "v0.5.0";
+    sha256 = "0l2n556fzgkgnw7rhwfsj7438qyid8y9sghlhd3is154dspg0p9v";
+  });
 
   haskellPackages = pkgs.haskellPackages.override {
     overrides = (self: super: {
