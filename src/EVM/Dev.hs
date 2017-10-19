d5eea8952f5c3c21a5261df318c5fd82a6a40b6c
Mikael Brockman <mikael@brockman.se>
2017-10-19 17:04:12 +0300

    EVM.Dev improvements for Haskell REPL developing

--- src/EVM/Dev.hs
+++ src/EVM/Dev.hs
@@ -8,6 +8,10 @@ import EVM.UnitTest
 
 import qualified EVM.Fetch
 import qualified EVM.TTY
+import qualified EVM.Facts     as Facts
+import qualified EVM.Facts.Git as Git
+
+import qualified Data.Map as Map
 
 loadDappInfo :: String -> String -> IO DappInfo
 loadDappInfo path file =
@@ -19,9 +23,44 @@ loadDappInfo path file =
         _ ->
           error "nope, sorry"
 
-ghciTty :: String -> String -> IO ()
-ghciTty root path =
+ghciTest :: String -> String -> Maybe String -> IO [Bool]
+ghciTest root path state =
   withCurrentDirectory root $ do
+    loadFacts <-
+      case state of
+        Nothing ->
+          pure id
+        Just repoPath -> do
+          facts <- Git.loadFacts (Git.RepoAt repoPath)
+          pure (flip Facts.apply facts)
+    let
+      opts = UnitTestOptions
+        { gasForCreating = defaultGasForCreating
+        , gasForInvoking = defaultGasForInvoking
+        , balanceForCreator = defaultBalanceForCreator
+        , balanceForCreated = defaultBalanceForCreated
+        , oracle = EVM.Fetch.zero
+        , verbose = False
+        , vmModifier = loadFacts
+        }
+    readSolc path >>=
+      \case
+        Just (contractMap, cache) -> do
+          let unitTests = findUnitTests (Map.elems contractMap)
+          mapM (runUnitTestContract opts contractMap cache) unitTests
+        Nothing ->
+          error ("Failed to read Solidity JSON for `" ++ path ++ "'")
+
+ghciTty :: String -> String -> Maybe String -> IO ()
+ghciTty root path state =
+  withCurrentDirectory root $ do
+    loadFacts <-
+      case state of
+        Nothing ->
+          pure id
+        Just repoPath -> do
+          facts <- Git.loadFacts (Git.RepoAt repoPath)
+          pure (flip Facts.apply facts)
     let
       testOpts = UnitTestOptions
         { gasForCreating = defaultGasForCreating
@@ -30,6 +69,6 @@ ghciTty root path =
         , balanceForCreated = defaultBalanceForCreated
         , oracle = EVM.Fetch.zero
         , verbose = False
-        , vmModifier = id
+        , vmModifier = loadFacts
         }
     EVM.TTY.main testOpts root path
