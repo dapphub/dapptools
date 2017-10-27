79a19a25f6e30ae72a815fa3ef1021c5d3c7b38f
Mikael Brockman <mikael@brockman.se>
2017-10-27 12:22:04 +0300

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,27 @@
+{ lib, stdenv, fetchFromGitHub, makeWrapper
+, seth, git, solc, shellcheck, nodejs, hevm, jshon, nix }:
+
+stdenv.mkDerivation rec {
+  name = "dapp-${version}";
+  version = "0.7.2";
+  src = ./.;
+
+  nativeBuildInputs = [makeWrapper shellcheck];
+  buildPhase = "true";
+  doCheck = true;
+  checkPhase = "make test";
+  makeFlags = ["prefix=$(out)"];
+  postInstall = let path = lib.makeBinPath [
+    nodejs solc git seth hevm jshon nix
+  ]; in ''
+    wrapProgram "$out/bin/dapp" --prefix PATH : "${path}"
+  '';
+
+  meta = {
+    description = "Simple tool for creating Ethereum-based dapps";
+    homepage = https://github.com/dapphub/dapp/;
+    maintainers = [stdenv.lib.maintainers.dbrock];
+    license = lib.licenses.gpl3;
+    inherit version;
+  };
+}
