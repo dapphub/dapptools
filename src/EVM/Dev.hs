00ce2268ab0d64764f5e101fe9d1193ae395a897
Mikael Brockman <mikael@brockman.se>
2017-10-31 17:15:03 +0200

    Add maximum pragma calculation & stuff to flatten
    Output is now finished, I think.  Next commit will move to its own file.

--- src/EVM/Dev.hs
+++ src/EVM/Dev.hs
@@ -10,7 +10,11 @@ import Control.Lens hiding (indices)
 
 import Data.Aeson
 import Data.Aeson.Lens
-import Data.Maybe (mapMaybe)
+import Data.Maybe (mapMaybe, isJust, catMaybes, fromMaybe)
+import Data.List (sort)
+import Data.Foldable (foldl', toList)
+import Data.Monoid ((<>))
+import Text.Read (readMaybe)
 
 import qualified EVM.Fetch
 import qualified EVM.TTY
@@ -26,6 +30,11 @@ import qualified Data.Graph.Inductive.Query.BFS as Fgl
 import qualified Data.Graph.Inductive.Query.DFS as Fgl
 
 import Data.Text (Text)
+import qualified Data.Text as Text
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
 
 loadDappInfo :: String -> String -> IO DappInfo
 loadDappInfo path file =
@@ -37,8 +46,8 @@ loadDappInfo path file =
         _ ->
           error "nope, sorry"
 
-flatten :: DappInfo -> Text -> IO ()
-flatten dapp target = do
+flatten :: DappInfo -> String -> Text -> IO ()
+flatten dapp path target = do
   let
     astMap  = view (dappSources . sourceAsts) dapp
     names   = Map.keys astMap
@@ -62,8 +71,79 @@ flatten dapp target = do
   case Map.lookup target indices of
     Nothing ->
       error "didn't find contract"
-    Just root -> do
-      print (reverse (Fgl.topsort' (Fgl.subgraph (Fgl.bfs root g) g)))
+    Just root ->
+      withCurrentDirectory path $ do
+        putStrLn $ "// hevm: flattened sources of " <> Text.unpack target
+        putStrLn (Text.unpack (maximalPragma (Map.elems astMap)))
+        mapM_
+          (\x -> do
+              src <- BS.readFile (Text.unpack x)
+              putStrLn ("////// " <> Text.unpack x <> "\n")
+              BS8.putStrLn (stripImportsAndPragmas src (astMap ! x)))
+          (reverse (Fgl.topsort' (Fgl.subgraph (Fgl.bfs root g) g)))
+
+maximalPragma :: [Value] -> Text
+maximalPragma asts =
+  case concatMap allPragmas asts of
+    [] -> error "No Solidity version pragmas"
+    xs -> "pragma solidity ^" <> Text.intercalate "." (map (Text.pack . show) (maximum xs)) <> ";\n"
+
+allPragmas :: Value -> [[Int]]
+allPragmas ast =
+  let
+    vs =
+      map toList . catMaybes $ map
+        (preview (key "attributes" . key "literals" . _Array))
+        (filter (flip nodeIs "PragmaDirective") (universe ast))
+    f = \case
+      [String "solidity", String _, String a, String b] ->
+        map
+          (fromMaybe
+             (error . Text.unpack $ "bad Solidity version: " <> a <> b)
+             . readAs)
+          (Text.splitOn "." (a <> b))
+      x ->
+        error ("oops: " ++ show x)
+  in map f vs
+
+nodeIs :: Value -> Text -> Bool
+nodeIs x t =
+  isJust (preview (key "src") x) && (preview (key "name" . _String) x == Just t)
+
+stripImportsAndPragmas :: ByteString -> Value -> ByteString
+stripImportsAndPragmas bs ast = stripAstNodes bs ast p
+  where
+    p x = nodeIs x "ImportDirective" || nodeIs x "PragmaDirective"
+
+stripAstNodes :: ByteString -> Value -> (Value -> Bool) -> ByteString
+stripAstNodes bs ast p =
+  cutRanges bs [astNodeByteRange node | node <- universe ast, p node]
+
+readAs :: Read a => Text -> Maybe a
+readAs = readMaybe . Text.unpack
+
+astNodeByteRange :: Value -> (Int, Int)
+astNodeByteRange v =
+  case preview (key "src" . _String) v of
+    Just (Text.splitOn ":" -> [readAs -> Just i, readAs -> Just n, _]) ->
+      (i, i + n)
+    _ ->
+      error "internal error: no source position for AST node"
+
+-- | Removes a set of non-overlapping ranges from a bytestring
+cutRanges :: ByteString -> [(Int, Int)] -> ByteString
+cutRanges bs (sort -> rs) = fst (foldl' f (bs, 0) rs)
+  where
+    f (bs', n) (i, j) =
+      ( cut bs' (i - n) (j - n)
+      , n - length ("/*  */" :: String)
+      )
+
+-- | Removes the bytes between two indices from a bytestring
+cut :: ByteString -> Int -> Int -> ByteString
+cut x i j =
+  let (a, b) = BS.splitAt i x
+  in a <> "/* " <> BS.take (j - i) b <> " */" <> BS.drop (j - i) b
 
 ghciTest :: String -> String -> Maybe String -> IO [Bool]
 ghciTest root path state =
