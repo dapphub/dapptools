e494bdf23c7483c6dc2d6fad9b9a447d982ecdf8
Mikael Brockman <mikael@brockman.se>
2017-11-24 16:58:19 +0200

    bump hevm unstable

--- overlay/upstream/master/hevm.nix
+++ overlay/upstream/master/hevm.nix
@@ -1,12 +1,13 @@
 { mkDerivation, abstract-par, aeson, ansi-wl-pprint, async, base
 , base16-bytestring, base64-bytestring, binary, brick, bytestring
 , cereal, containers, cryptonite, data-dword, deepseq, directory
-, fgl, filepath, ghci-pretty, here, HUnit, lens, lens-aeson
-, megaparsec, memory, monad-par, mtl, multiset, operational
-, optparse-generic, process, QuickCheck, quickcheck-text, readline
-, restless-git, rosezipper, scientific, stdenv, tasty, tasty-hunit
-, tasty-quickcheck, temporary, text, text-format, time
-, transformers, tree-view, unordered-containers, vector, vty, wreq
+, fgl, filepath, ghci-pretty, haskeline, here, HUnit, lens
+, lens-aeson, megaparsec, memory, monad-par, mtl, multiset
+, operational, optparse-generic, process, QuickCheck
+, quickcheck-text, readline, regex-tdfa, restless-git, rosezipper
+, scientific, stdenv, tasty, tasty-hunit, tasty-quickcheck
+, temporary, text, text-format, time, transformers, tree-view
+, unordered-containers, vector, vty, wreq
 }:
 mkDerivation {
   pname = "hevm";
@@ -19,7 +20,7 @@ mkDerivation {
     abstract-par aeson ansi-wl-pprint base base16-bytestring
     base64-bytestring binary brick bytestring cereal containers
     cryptonite data-dword deepseq directory fgl filepath ghci-pretty
-    lens lens-aeson megaparsec memory monad-par mtl multiset
+    haskeline lens lens-aeson megaparsec memory monad-par mtl multiset
     operational optparse-generic process QuickCheck quickcheck-text
     readline restless-git rosezipper scientific temporary text
     text-format time transformers tree-view unordered-containers vector
@@ -30,7 +31,8 @@ mkDerivation {
     binary brick bytestring containers cryptonite data-dword deepseq
     directory filepath ghci-pretty lens lens-aeson memory mtl
     optparse-generic process QuickCheck quickcheck-text readline
-    temporary text text-format unordered-containers vector vty
+    regex-tdfa temporary text text-format unordered-containers vector
+    vty
   ];
   testHaskellDepends = [
     base binary bytestring ghci-pretty here HUnit lens mtl QuickCheck
