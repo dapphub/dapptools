c4c848d0c28e1b9ad999ec1fce8f33f70e928cbd
Mikael Brockman <mikael@brockman.se>
2017-12-11 18:28:49 +0200

    Update hevm master with ethjet support

--- overlay/upstream/master/hevm.nix
+++ overlay/upstream/master/hevm.nix
@@ -1,7 +1,7 @@
 { mkDerivation, abstract-par, aeson, ansi-wl-pprint, async, base
 , base16-bytestring, base64-bytestring, binary, brick, bytestring
 , cereal, containers, cryptonite, data-dword, deepseq, directory
-, fgl, filepath, ghci-pretty, haskeline, here, HUnit, lens
+, ethjet, fgl, filepath, ghci-pretty, haskeline, here, HUnit, lens
 , lens-aeson, megaparsec, memory, monad-par, mtl, multiset
 , operational, optparse-generic, process, QuickCheck
 , quickcheck-text, readline, regex-tdfa, restless-git, rosezipper
@@ -19,12 +19,12 @@ mkDerivation {
   libraryHaskellDepends = [
     abstract-par aeson ansi-wl-pprint base base16-bytestring
     base64-bytestring binary brick bytestring cereal containers
-    cryptonite data-dword deepseq directory fgl filepath ghci-pretty
-    haskeline lens lens-aeson megaparsec memory monad-par mtl multiset
-    operational optparse-generic process QuickCheck quickcheck-text
-    readline restless-git rosezipper scientific temporary text
-    text-format time transformers tree-view unordered-containers vector
-    vty wreq
+    cryptonite data-dword deepseq directory ethjet fgl filepath
+    ghci-pretty haskeline lens lens-aeson megaparsec memory monad-par
+    mtl multiset operational optparse-generic process QuickCheck
+    quickcheck-text readline restless-git rosezipper scientific
+    temporary text text-format time transformers tree-view
+    unordered-containers vector vty wreq
   ];
   executableHaskellDepends = [
     aeson ansi-wl-pprint async base base16-bytestring base64-bytestring
