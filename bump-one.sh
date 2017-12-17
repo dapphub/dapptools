fb5b98f69589dc5d46b80632fd53b4128f56a0b2
Mikael Brockman <mikael@brockman.se>
2017-12-17 12:19:26 +0200

    parallel bumper

---
+++ bump-one.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -eu
+
+exec > >(sed "s/^/${1/\//\\\/}: /") 2> >(sed >&2 "s/^/${1/\//\\\/}: /")
+
+GET() {
+  curl -s -H "Authorization: token ${GITHUB_TOKEN?Need OAuth token}" "$@"
+}
+
+echo "bumping"
+pkg="$1"
+name=$(basename "$pkg")
+tag=$(GET https://api.github.com/repos/"$pkg"/tags | jshon -e 0)
+version=$(jshon -e name -u <<<"$tag")
+taghash=$(jshon -e commit -e sha -u <<<"$tag")
+head=$(GET https://api.github.com/repos/"$pkg"/commits | jshon -e 0)
+headhash=$(jshon -e sha -u <<<"$head")
+
+echo "stable $version, master $headhash"
+
+tagsha256=$(
+  nix-prefetch-url \
+    --unpack \
+    https://github.com/"$pkg"/archive/"$version".tar.gz 2>/dev/null)
+
+headsha256=$(
+  nix-prefetch-url \
+    --unpack \
+    https://github.com/"$pkg"/archive/"$headhash".tar.gz 2>/dev/null)
+
+tree=$(GET https://api.github.com/repos/"$pkg"/git/trees/"$taghash")
+nix=$(jq -r <<<"$tree" '.tree | .[] | select(.path == "default.nix") | .url')
+GET "$nix" | jshon -e content -u | base64 -d > overlay/upstream/stable/$name.nix
+
+tree=$(GET https://api.github.com/repos/"$pkg"/git/trees/"$headhash")
+nix=$(jq -r <<<"$tree" '.tree | .[] | select(.path == "default.nix") | .url')
+GET "$nix" | jshon -e content -u | base64 -d > overlay/upstream/master/$name.nix
+
+(
+  flock 9 || exit 1
+  json=$(cat bump.json)
+  json=$(
+    jshon <<<"$json" -n {} \
+      -s "${version#v}" -i version \
+      -n {} \
+      -s "$headhash" -i rev \
+      -s "$headsha256" -i sha256 \
+      -s "$(dirname "$pkg")" -i owner \
+      -s "$name" -i repo \
+      -i master \
+      -n {} \
+      -s "$taghash" -i rev \
+      -s "$tagsha256" -i sha256 \
+      -s "$(dirname "$pkg")" -i owner \
+      -s "$name" -i repo \
+      -i stable \
+      -i "$name"
+  )
+  echo "$json" >bump.json
+) 9>/tmp/dapphub-bump.lock
+
+echo "done"
--- bump.sh
+++ bump.sh
@@ -1,75 +1,25 @@
 #!/usr/bin/env bash
 set -e
-json="{}"
-add() { json=$(jshon "$@" <<<"$json"); }
 
-rm -rf overlay/upstream
 mkdir -p overlay/upstream/{master,stable}
+echo '{}' >bump.json
 
-GET() {
-  curl -s -H "Authorization: token ${GITHUB_TOKEN?Need OAuth token}" "$@"
-}
+pkgs=(
+  dapphub/dapp
+  mbrock/jays
+  dapphub/hevm
+  dapphub/seth
+  dapphub/ethsign
+  lessrest/restless-git
+  dapphub/libethjet
+  dapphub/libethjet-haskell
+  mbrock/symbex
+  makerdao/setzer
+#  makerdao/dai-cli
+  mbrock/oasis-orders
+)
 
-package() {
-  echo -n bumping "$1 "
-  pkg="$1"
-  name=$(basename "$pkg")
-  tag=$(GET https://api.github.com/repos/"$pkg"/tags | jshon -e 0)
-  version=$(jshon -e name -u <<<"$tag")
-  taghash=$(jshon -e commit -e sha -u <<<"$tag")
-  head=$(GET https://api.github.com/repos/"$pkg"/commits | jshon -e 0)
-  headhash=$(jshon -e sha -u <<<"$head")
-  echo -n "stable $version, master $headhash"
-  tagsha256=$(
-    nix-prefetch-url \
-      --unpack \
-      https://github.com/"$pkg"/archive/"$version".tar.gz 2>/dev/null)
-  headsha256=$(
-    nix-prefetch-url \
-      --unpack \
-      https://github.com/"$pkg"/archive/"$headhash".tar.gz 2>/dev/null)
-  add -n {} \
-      -s "${version#v}" -i version \
-      -n {} \
-      -s "$headhash" -i rev \
-      -s "$headsha256" -i sha256 \
-      -s "$(dirname "$pkg")" -i owner \
-      -s "$name" -i repo \
-      -i master \
-      -n {} \
-      -s "$taghash" -i rev \
-      -s "$tagsha256" -i sha256 \
-      -s "$(dirname "$pkg")" -i owner \
-      -s "$name" -i repo \
-      -i stable \
-      -i "$name"
+printf "%s\n" "${pkgs[@]}" | xargs -L 1 --max-procs=8 ./bump-one.sh
 
-  tree=$(GET https://api.github.com/repos/"$pkg"/git/trees/"$taghash")
-  nix=$(jq -r <<<"$tree" '.tree | .[] | select(.path == "default.nix") | .url')
-  GET "$nix" | jshon -e content -u | base64 -d > overlay/upstream/stable/$name.nix
-
-  tree=$(GET https://api.github.com/repos/"$pkg"/git/trees/"$headhash")
-  nix=$(jq -r <<<"$tree" '.tree | .[] | select(.path == "default.nix") | .url')
-  GET "$nix" | jshon -e content -u | base64 -d > overlay/upstream/master/$name.nix
-
-  echo
-}
-
-package mbrock/jays
-package mbrock/symbex
-package makerdao/setzer
-package lessrest/restless-git
-
-package dapphub/hevm
-package dapphub/seth
-package dapphub/ethsign
-
-package dapphub/libethjet
-package dapphub/libethjet-haskell
-
-package mbrock/oasis-orders
-
-# This one is slow because of node_modules, lol.
-package dapphub/dapp
-
-echo "$json" | jq . > overlay/versions.json
+jq . < ./bump.json > overlay/versions.json
+rm bump.json
