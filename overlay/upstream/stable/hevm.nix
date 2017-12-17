e12cca13d432ac7069ad141f596d242a63284c66
Mikael Brockman <mikael@brockman.se>
2017-12-17 16:51:01 +0200

    bump hevm

--- overlay/upstream/stable/hevm.nix
+++ overlay/upstream/stable/hevm.nix
@@ -1,7 +1,7 @@
 { mkDerivation, abstract-par, aeson, ansi-wl-pprint, async, base
 , base16-bytestring, base64-bytestring, binary, brick, bytestring
 , cereal, containers, cryptonite, data-dword, deepseq, directory
-, fgl, filepath, ghci-pretty, haskeline, here, HUnit, lens
+, ethjet, fgl, filepath, ghci-pretty, haskeline, here, HUnit, lens
 , lens-aeson, megaparsec, memory, monad-par, mtl, multiset
 , operational, optparse-generic, process, QuickCheck
 , quickcheck-text, readline, regex-tdfa, restless-git, rosezipper
@@ -11,7 +11,7 @@
 }:
 mkDerivation {
   pname = "hevm";
-  version = "0.12.1";
+  version = "0.12.2";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
@@ -19,12 +19,12 @@ mkDerivation {
   libraryHaskellDepends = [
     abstract-par aeson ansi-wl-pprint base base16-bytestring
     base64-bytestring binary brick bytestring cereal containers
-    cryptonite data-dword deepseq directory fgl filepath ghci-pretty
-    haskeline lens lens-aeson megaparsec memory monad-par mtl multiset
-    operational optparse-generic process QuickCheck quickcheck-text
-    readline restless-git rosezipper scientific temporary text
-    text-format time transformers tree-view unordered-containers vector
-    vty wreq
+    cryptonite data-dword deepseq directory ethjet fgl filepath
+    ghci-pretty haskeline lens lens-aeson megaparsec memory monad-par
+    mtl multiset operational optparse-generic process QuickCheck
+    quickcheck-text readline restless-git rosezipper scientific
+    temporary text text-format time transformers tree-view
+    unordered-containers vector vty wreq
   ];
   executableHaskellDepends = [
     aeson ansi-wl-pprint async base base16-bytestring base64-bytestring
