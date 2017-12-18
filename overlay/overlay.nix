f06fa6e5a739c6a04e2283a42012893b8b806869
Mikael Brockman <mikael@brockman.se>
2017-12-19 01:17:26 +0200

    oasis-2017-12 in dapp-which

--- overlay/overlay.nix
+++ overlay/overlay.nix
@@ -101,6 +101,7 @@ in rec {
       table[weth-2017-12]=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
       table[mkr-redeemer-2017-12]=0x642AE78FAfBB8032Da552D619aD43F1D81E4DD7C
       table[oasis-2017-09]=0x3Aa927a97594c3ab7d7bf0d47C71c3877D1DE4A1
+      table[oasis-2017-12]=0x14FBCA95be7e99C15Cc2996c6C9d841e54B79425
       table[sai-2017-07]=0x59aDCF176ED2f6788A41B8eA4c4904518e62B6A4
 
       if [ "$#" -eq 0 ]; then
