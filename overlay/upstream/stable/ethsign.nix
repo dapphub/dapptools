947f2b1088c0ee014719931d294f2a5cefcd4471
Mikael Brockman <mikael@brockman.se>
2018-01-11 13:37:06 +0200

    ethsign v0.8.2

--- overlay/upstream/stable/ethsign.nix
+++ overlay/upstream/stable/ethsign.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   name = "ethsign-${version}";
-  version = "0.8";
+  version = "0.8.2";
 
   goPackagePath = "github.com/dapphub/ethsign";
   hardeningDisable = ["fortify"];
