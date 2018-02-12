32cee265663c9b1f3222c43a745287448a63a0b8
Daniel Brockman <daniel@brockman.se>
2018-02-12 05:43:23 +0000

    syntax

--- overlay/dapp-which.nix
+++ overlay/dapp-which.nix
@@ -12,21 +12,21 @@ in bashScript {
     ${builtins.concatStringsSep "\n"
         (lib.mapAttrsToList (k: v: "table[${k}]=\"${v}\"") contracts)}
 
-    if [ "$#" -eq 0 ]; then
+    if [[ $# = 0 ]]; then
       for k in "''${!table[@]}"; do
         echo "$k" "''${table[$k]}"
       done | sort | awk '{ printf("%32s  %s\n", $1, $2) }'
-    elif [ "$#" -eq 1 ]; then
-      if [ ''${table[$1]+_} ]; then
+    elif [[ $# = 1 ]]; then
+      if [[ ''${table[$1]+_} ]]; then
         echo "''${table[$1]}"
       else
         for k in "''${!table[@]}"; do
-          if [ "''${table[$k],,}" == "''${1,,}" ]; then
+          if [[ ''${table[$k],,} = ''${1,,} ]]; then
             echo "$k"
             exit
           fi
         done
-        echo >&2 "dapp-which: don't know $1"
+        echo >&2 "dapp-which: don't know \`$1'"
       fi
     else
       echo >&2 "usage: dapp-which [CONTRACT-ID | ADDRESS]"
