fa16739a4c1cf4f913ccd4ba39a49f5a37ef1b20
Mikael Brockman <mikael@brockman.se>
2018-05-05 16:28:02 +0300

    <nixpkgs> -> <dapphub>

--- hevm.nix
+++ hevm.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import <dapphub> {};
   haskellPackages = pkgs.haskellPackages;
   profiling = haskellPackages.override {
     overrides = self: super: {
