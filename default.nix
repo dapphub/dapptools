7d50fea5731dfce90549c8e8a52b067a373ba9fb
Mikael Brockman <mbrock@Mikaels-MacBook.local>
2018-10-05 18:43:20 +0300

    Bump nixpkgs
    This fixes issues with macOS Mojave...

--- default.nix
+++ default.nix
@@ -4,8 +4,8 @@ let
   nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "4b649a99d8461c980e7028a693387dc48033c1f7";
-    sha256 = "0iy2gllj457052wkp20baigb2bnal9nhyai0z9hvjr3x25ngck4y";
+    rev = "8c2c14ac392e5b96a1b3d12e16ba0439689024c7";
+    sha256 = "0x3b0ml7gxc9y28y4l64mx6w5582ncks0rca00ikn1pqffffvbxi";
   };
 in
   # Now return the Nixpkgs configured to use our overlay.
