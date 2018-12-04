ac0d44fa1fc5a4aca4f800122a55850df21e3dfc
Mariano Conti <nanexcool@gmail.com>
2018-12-04 10:19:43 -0300

    Build solc. Still doesn't work on Mac

---
+++ solc.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+let
+  system = (system: (import ./default.nix { inherit system; }));
+  linux = system "x86_64-linux";
+  darwin = system "x86_64-darwin";
+
+  # These packages should always work and be available in the binary cache.
+  stable = dist: with dist.pkgs; {
+    inherit solc;
+  };
+
+in {
+  dapphub.linux.stable = stable linux;
+  dapphub.darwin.stable = stable darwin;
+}
