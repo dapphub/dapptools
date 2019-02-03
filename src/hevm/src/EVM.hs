ddc27b87e8d7bab3ac7edd73eca19d56b8e7344b
Mariano Conti <nanexcool@gmail.com>
2019-02-03 16:08:48 -0300

    First implementation of SHL and SHR

--- src/hevm/src/EVM.hs
+++ src/hevm/src/EVM.hs
@@ -26,7 +26,7 @@ import qualified EVM.Precompiled
 
 import Data.Binary.Get (runGetOrFail)
 import Data.Bits (bit, testBit, complement)
-import Data.Bits (xor, shiftR, (.&.), (.|.), FiniteBits (..))
+import Data.Bits (xor, shiftL, shiftR, (.&.), (.|.), FiniteBits (..))
 import Data.Text (Text)
 import Data.Word (Word8, Word32)
 
@@ -485,14 +485,15 @@ exec1 = do
             0xff .&. shiftR x (8 * (31 - num n))
 
         -- op: SHL
-        0x1b -> error "SHL not implemented"
-        -- 0x1b -> stackOp2 (const g_verylow) $
+        0x1b -> stackOp2 (const g_verylow) $ \case
+          (n, _) | n >= 256 -> 0
+          (n, x) -> shiftL x (num n)
         -- op: SHR
-        0x1c -> error "SHR not implemented"
-        -- 0x1c -> stackOp2 (const g_verylow) $
+        0x1c -> stackOp2 (const g_verylow) $ \case
+          (n, _) | n >= 256 -> 0
+          (n, x) -> shiftR x (num n)
         -- op: SAR
         0x1d -> error "SAR not implemented"
-        -- 0x1d -> stackOp2 (const g_verylow) $
 
         -- op: SHA3
         0x20 ->
