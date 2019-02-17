0e1314d3c07bef086d55218860074ba4b6572a24
Mikael Brockman <mikael@brockman.se>
2019-02-17 15:51:18 +0200

    hevm: start to bind libff to ethjet
    There's no actual functionality yet, but the building and linking
    setup seems to be working.

---
+++ src/hevm/ethjet/ethjet-ff.cc
@@ -0,0 +1,7 @@
+#include <libff/algebra/curves/bn128/bn128_pairing.hpp>
+
+extern "C" {
+  // Do stuff here to make relevant functionality available to
+  // `ethjet.c' -- I just have no clue what's going on with this
+  // cryptic crypto nonsense.
+}
