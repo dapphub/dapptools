58324f039d02a35c22c9af4ec5fe3a492a421fcb
Lev Livnev <lev@liv.nev.org.uk>
2019-04-03 22:32:56 +0100

    src/hevm/shell.nix: add pkgs.solc, needed for tests

--- src/hevm/shell.nix
+++ src/hevm/shell.nix
@@ -15,7 +15,7 @@ let
     haskellPackages.callPackage (import ./default.nix) {
       inherit (pkgs) secp256k1;
     }
-  ) [pkgs.git pkgs.cabal-install pkgs.cabal2nix];
+  ) [pkgs.git pkgs.cabal-install pkgs.cabal2nix pkgs.solc];
 
 in
   if pkgs.lib.inNixShell
