cb9cbcdcbe3d7449d122c4543013fa884517113a
Lorenzo Manacorda <lorenzo@mailbox.org>
2019-05-04 11:43:36 +0200

    *: fix versions for dapp and seth
    They hadn't been updated in sync with the tags.

--- src/dapp/default.nix
+++ src/dapp/default.nix
@@ -4,7 +4,7 @@
 
 stdenv.mkDerivation rec {
   name = "dapp-${version}";
-  version = "0.17.0";
+  version = "0.18.1";
   src = ./.;
 
   nativeBuildInputs = [makeWrapper shellcheck coreutils nodejs];
--- src/seth/default.nix
+++ src/seth/default.nix
@@ -3,7 +3,7 @@
 
 stdenv.mkDerivation rec {
   name = "seth-${version}";
-  version = "0.7.0";
+  version = "0.8.0";
   src = ./.;
 
   nativeBuildInputs = [makeWrapper];
