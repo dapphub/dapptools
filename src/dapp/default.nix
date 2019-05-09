a0498955f0463bec807188bfb36a1ace6fe97017
Lorenzo Manacorda <lorenzo@mailbox.org>
2019-05-09 17:38:19 +0200

    dapp: 0.19.0

--- src/dapp/default.nix
+++ src/dapp/default.nix
@@ -4,7 +4,7 @@
 
 stdenv.mkDerivation rec {
   name = "dapp-${version}";
-  version = "0.18.1";
+  version = "0.19.0";
   src = ./.;
 
   nativeBuildInputs = [makeWrapper shellcheck coreutils nodejs];
