de39e1f3e63f8800a22e19d92d62077fe89f34c6
Lev Livnev <lev@liv.nev.org.uk>
2019-05-12 21:50:14 +0100

    hevm: ethjet-ff.cc: add ECMUL implementation

--- src/hevm/ethjet/ethjet-ff.cc
+++ src/hevm/ethjet/ethjet-ff.cc
@@ -169,7 +169,133 @@ extern "C" {
   ethjet_ecmul (ethjet_ff_context *ctx,
                 uint8_t *in, size_t in_size,
                 uint8_t *out, size_t out_size) {
-    return 0;
+
+    if (in_size != 96) {
+      // printf("in_size=%d\n", int(in_size));
+      return 0;
+    }
+    if (out_size != 64) {
+      // printf("out_size=%d\n", int(out_size));
+      return 0;
+    }
+
+    mpz_t n_data;
+    mpz_t ax_data;
+    mpz_t ay_data;
+    mpz_init(n_data);
+    mpz_init(ax_data);
+    mpz_init(ay_data);
+    mpz_import(ax_data, 32, 1, sizeof(in[0]), 1, 0, in);
+    mpz_import(ay_data, 32, 1, sizeof(in[0]), 1, 0, in+32);
+    mpz_import(n_data,  32, 1, sizeof(in[0]), 1, 0, in+64);
+
+    // gmp_printf("ax=%Zd, ay=%Zd\n", ax_data, ay_data);
+    // gmp_printf("n=%Zd\n", n_data);
+
+    init_alt_bn128_params();
+    const mp_size_t limbs      = alt_bn128_q_limbs;
+    const alt_bn128_Fq Fq_zero = alt_bn128_Fq::zero();
+    const alt_bn128_Fq Fq_one  = alt_bn128_Fq::one();
+    mpz_t q;
+    mpz_init(q);
+    alt_bn128_modulus_q.to_mpz(q);
+    // gmp_printf("q=%Zd\n", q);
+
+    // reduce n mod q
+    // mpz_mod(n_data, n_data, q);
+    // gmp_printf("after reducing n=%Zd\n", n_data);
+
+    // check scalar and coordinates are in range
+    // n.b. the scalar needn't be in range
+    if ((mpz_cmp(ax_data, q) >= 0) ||
+        (mpz_cmp(ay_data, q) >= 0)) {
+      // printf("One of the coordinates was out of range!\n");
+      return 0;
+    }
+
+    const alt_bn128_Fq n = Fp_model<alt_bn128_q_limbs, alt_bn128_modulus_q>(bigint<limbs>(n_data));
+    const alt_bn128_Fq ax = Fp_model<alt_bn128_q_limbs, alt_bn128_modulus_q>(bigint<limbs>(ax_data));
+    const alt_bn128_Fq ay = Fp_model<alt_bn128_q_limbs, alt_bn128_modulus_q>(bigint<limbs>(ay_data));
+
+    alt_bn128_G1 a;
+    // create curve point from affine coordinates
+    // the point at infinity (0,0) is a special case
+    if (ax.is_zero() && ay.is_zero()) {
+      a = alt_bn128_G1(Fq_zero, Fq_one, Fq_zero);
+    }
+    else {
+      a = alt_bn128_G1(ax, ay, Fq_one);
+    }
+
+    // printf("a=");
+    // a.print();
+
+    // check point is on curve
+    if (! a.is_well_formed()) {
+      // printf("a is ill-formed!\n");
+      return 0;
+    }
+
+    alt_bn128_G1 na = n * a;
+    // remember to convert the Jacobian (projective)
+    // coordinates back to affine before extracting
+    na.to_affine_coordinates();
+    // printf("after normalisation, n*a=");
+    // na.print();
+
+    mpz_t naX;
+    mpz_t naY;
+    size_t naX_size;
+    size_t naY_size;
+    mpz_init(naX);
+    mpz_init(naY);
+    // point at infinity is represented as (0,0)
+    // so leave the arrays empty in that case
+    if (!na.is_zero()) {
+      na.X.as_bigint().to_mpz(naX);
+      na.Y.as_bigint().to_mpz(naY);
+    }
+    // gmp_printf("sum.X=%Zd, sum.Y=%Zd\n", sumX, sumY);
+
+    uint8_t *naX_arr = (uint8_t *)mpz_export(NULL, &naX_size, 1, 1, 1, 0, naX);
+    uint8_t *naY_arr = (uint8_t *)mpz_export(NULL, &naY_size, 1, 1, 1, 0, naY);
+
+    if (naX_size > 32 || naY_size > 32) {
+      // printf("Output is too large!\n");
+      return 0;
+    }
+    // printf("sumX_size=%d, sumY_size=%d\n", int(sumX_size), int(sumY_size));
+    // copy the result to the output buffer
+    for (int i = 1; i <= 32; i++) {
+      if (i <= naX_size)
+        out[32-i] = naX_arr[naX_size-i];
+      else
+        out[32-i] = 0;
+    }
+    for (int i = 1; i <= 32; i++) {
+      if (i <= naY_size)
+        out[64-i] = naY_arr[naY_size-i];
+      else
+        out[64-i] = 0;
+    }
+
+    // printf("Sum was: 0x");
+    // for (int i = 0; i < 32; i++) {
+    //   printf("%02x", sumX_arr[i]);
+    // }
+    // printf("\n");
+    // for (int i = 0; i < 32; i++) {
+    //   printf("%02x", sumY_arr[i]);
+    // }
+    // printf("\n");
+
+    // printf("Output was: 0x");
+    // for (int i = 0; i < 64; i++) {
+    //   printf("%02x", out[i]);
+    // }
+    // printf("\n");
+
+    return 1;
   }
 
   int
