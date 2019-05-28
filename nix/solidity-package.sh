a22c4484d3e9c2f11f125bc75c3e106da8d837cf
Christopher Fredén <me@icetan.org>
2019-05-28 19:50:38 +0200

    Change `solcFlags` env variable from array to regular string

--- nix/solidity-package.sh
+++ nix/solidity-package.sh
@@ -1,7 +1,6 @@
 source $stdenv/setup
 unpackPhase
 
-IFS=" " read -r -a opts <<<"$solcFlags"
 jsonopts=--combined-json=abi,bin,bin-runtime,srcmap,srcmap-runtime,ast
 
 export DAPP_SRC=$src
@@ -12,9 +11,9 @@ find "$DAPP_SRC" -name '*.sol' | while read -r x; do
   dir=${dir#$DAPP_SRC}
   dir=${dir#/}
   mkdir -p "$DAPP_OUT/$dir"
-  (set -x; solc --overwrite $REMAPPINGS --allow-paths $DAPP_SRC "${opts[@]}" --abi --bin --bin-runtime -o "$DAPP_OUT/$dir" "$x")
+  (set -x; solc --overwrite $REMAPPINGS --allow-paths $DAPP_SRC $solcFlags --abi --bin --bin-runtime -o "$DAPP_OUT/$dir" "$x")
   json_file=$DAPP_OUT/$dir/${x##*/}.json
-  (set -x; solc $REMAPPINGS --allow-paths $DAPP_SRC "${opts[@]}" $jsonopts "$x" >"$json_file")
+  (set -x; solc $REMAPPINGS --allow-paths $DAPP_SRC $solcFlags $jsonopts "$x" >"$json_file")
 done
 
 mkdir lib
