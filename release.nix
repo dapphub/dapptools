3da0451852147182c5f7788534af9c4cd0975dd6
dc <dc@log-ix.com>
2019-08-06 11:08:46 +1200

    hevm: bump ethereum tests to latest commit

--- release.nix
+++ release.nix
@@ -8,15 +8,17 @@ let
   ethereum-test-suite = x: x.fetchFromGitHub {
     owner = "ethereum";
     repo = "tests";
-    rev = "7e361956bd68f5cac72fe41f29e5734ee94ae2de";
-    sha256 = "0l5qalgbscr77vjhyf7b542055wnp4pddpfslnypp5sqws5w940w";
+    rev = "da6d391922cb0e3c6bda24871c89d33bc815c1dc";
+    sha256 = "06h3hcsm09kp4hzq5sm9vqkmvx2nvgbh5i788qnqh5iiz9fpaa9k";
   };
 
-  hevmTestReport = x: x.runCommand "hevm-test-report" {} ''
+  hevmCompliance = x: x.runCommand "hevm-compliance" {} ''
     mkdir -p $out/nix-support
     export PATH=${x.pkgs.hevm}/bin:$PATH
-    ${x.pkgs.hevm}/bin/hevm vm-test-report \
-      --tests ${ethereum-test-suite x} > $out/index.html
+    ${x.pkgs.hevm}/bin/hevm compliance \
+      --tests ${ethereum-test-suite x} \
+      --skip modexp \
+      --html > $out/index.html
     echo report testlog $out index.html > $out/nix-support/hydra-build-products
   '';
 
@@ -36,7 +38,7 @@ let
     inherit setzer;
     inherit token;
 
-    hevm-test-report = hevmTestReport dist;
+    hevm-compliance = hevmCompliance dist;
   # the union is necessary because nix-build does not evaluate sets
   # recursively, and `solc-versions` is a set
   } // dist.pkgs.solc-versions ;
