1499509492eeec35b36b6b9d797add04e42f158a
Martin Lundfall <martin.lundfall@protonmail.com>
2020-04-21 20:48:07 +0200

    add solc:0.6.6 support

--- nix/solc-versions.nix
+++ nix/solc-versions.nix
@@ -42,5 +42,6 @@ rec {
   unreleased = {
     solc_0_5_16 = { rev = "725f92e016497dc351f84ff3d2c0c0538676f287"; sha256 = "1qwh0246piiwxxd80dzhya6x1fd2w2kj2rf80y83qa2v03zczqd1"; };
     solc_0_6_0  = { rev = "2342685388c0bad7163483c0b96c64f2dae817a3"; sha256 = "0ahqhw47y9xd3gwcxiazxj5ijiahgb4b69bvxja227rfpi9yvx60"; };
+    solc_0_6_6  = { rev = "76d3d5826d9bff8b3952979514e0bc670bfbd8be"; sha256 = "0h3x3ldbv64li0aqajf2660gvzabgkkcjbbnq96l30llmxm5nrid"; };
   };
 }
