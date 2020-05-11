e2d8412d867b2b88be42d592887e75d582d14789
MrChico <martin.lundfall@protonmail.com>
2020-05-11 10:15:18 +0200

    UnitTest: reset state before tracing (#371)

--- src/hevm/src/EVM/UnitTest.hs
+++ src/hevm/src/EVM/UnitTest.hs
@@ -126,9 +126,9 @@ initializeUnitTest UnitTestOptions { .. } = do
     env . contracts . ix addr . balance += w256 (testBalanceCreate testParams)
 
   -- Initialize the test contract
-  Stepper.evm (popTrace >> pushTrace (EntryTrace "initialize test"))
   Stepper.evm $
     setupCall testParams addr "setUp()" emptyAbi
+  Stepper.evm (popTrace >> pushTrace (EntryTrace "initialize test"))
 
   Stepper.note "Running `setUp()'"
 
