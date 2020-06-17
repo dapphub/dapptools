cc5ffa21b7540268c16c7b7d3d497951f9290c29
Martin Lundfall <martin.lundfall@protonmail.com>
2020-06-18 00:26:33 +0200

    move equivalence checking out to symbolic, more customization options for symexec cli endpoint

--- src/hevm/src/EVM/Symbolic.hs
+++ src/hevm/src/EVM/Symbolic.hs
@@ -11,6 +11,7 @@ import EVM hiding (Query, push)
 import qualified EVM as EVM
 import EVM.Exec
 import EVM.Op
+import Options.Generic as Options
 import qualified EVM.Fetch as Fetch
 import EVM.ABI
 import EVM.Stepper (Stepper)
@@ -44,13 +45,13 @@ import Control.Applicative
 import Data.Maybe (isJust, fromJust)
 import Data.Foldable (fold)
 
-mkSymbolicContract :: ContractCode -> SArray (WordN 256) (WordN 256) -> Contract
+mkSymbolicContract :: ContractCode -> Storage -> Contract
 mkSymbolicContract theContractCode store = Contract
   { _contractcode = theContractCode
   , _codehash =
     if BS.null theCode then 0 else
       keccak (stripBytecodeMetadata theCode)
-  , _storage  = Symbolic store
+  , _storage  = store
   , _balance  = 0
   , _nonce    = 0
   , _opIxMap  = mkOpIxMap theCode
@@ -61,8 +62,8 @@ mkSymbolicContract theContractCode store = Contract
             InitCode b    -> b
             RuntimeCode b -> b
 
-abstractVM :: Maybe Text -> ByteString ->  Query VM
-abstractVM signature' x = do
+abstractVM :: Maybe Text -> ByteString -> StorageModel -> Query VM
+abstractVM signature' x storagemodel = do
   (cd', cdlen, cdconstraint) <-
     case signature' of
       Nothing -> do cd <- sbytes256
@@ -70,11 +71,14 @@ abstractVM signature' x = do
                     return (cd, len, len .<= 1024)
       Just sign -> do (input,len) <- symAbiArg $ fromJust (parseFunArgs sign)
                       return (litBytes (sig sign) <> input, len + 4, sTrue)
-  symstore <- freshArray_ Nothing
+  symstore <- case storagemodel of
+    SymbolicS -> Symbolic <$> freshArray_ Nothing
+    InitialS -> Symbolic <$> freshArray_ (Just 0)
+    ConcreteS -> return $ Concrete mempty
   c <- SAddr <$> freshVar_
   return $ loadSymVM x symstore c (cd', cdlen) & over pathConditions ((<>) [cdconstraint])
 
-loadSymVM :: ByteString -> SArray (WordN 256) (WordN 256) -> SAddr -> ([SWord 8], SWord 32) -> VM
+loadSymVM :: ByteString -> Storage -> SAddr -> ([SWord 8], SWord 32) -> VM
 loadSymVM x initStore addr calldata =
     (makeVm $ VMOpts
     { vmoptContract = mkSymbolicContract (RuntimeCode x) initStore
@@ -195,39 +199,98 @@ type Precondition = [SWord 8] -> SBool
 type Postcondition = (VM, VM) -> SBool
 
 checkAssert :: ContractCode -> Maybe Integer -> Maybe Text -> Query (Either (VM, [VM]) ByteString)
-checkAssert code maxIter signature' = let post = Just $ \(_, output) ->
-                                            case view result output of
-                                              Just (EVM.VMFailure (EVM.UnrecognizedOpcode 254)) -> sFalse
-                                              _ -> sTrue
-                                      in verify code maxIter signature' (const sTrue) post
+checkAssert code maxIter signature' = verifyContract code maxIter signature' SymbolicS (const sTrue) (Just checkAssertions)
 
-verify :: ContractCode -> Maybe Integer -> Maybe Text -> Precondition -> Maybe Postcondition -> Query (Either (VM, [VM]) ByteString)
-verify (RuntimeCode runtimecode) maxIter signature' pre maybepost = do
-    preStateRaw <- abstractVM signature' runtimecode
+checkAssertions :: Postcondition
+checkAssertions (_, output) = case view result output of
+  Just (EVM.VMFailure (EVM.UnrecognizedOpcode 254)) -> sFalse
+  _ -> sTrue
+
+data StorageModel = ConcreteS | SymbolicS | InitialS
+  deriving (Read, Show)
+
+instance ParseField StorageModel
+
+verifyContract :: ContractCode -> Maybe Integer -> Maybe Text -> StorageModel -> Precondition -> Maybe Postcondition -> Query (Either (VM, [VM]) ByteString)
+verifyContract (RuntimeCode runtimecode) maxIter signature' storagemodel pre maybepost = do
+    preStateRaw <- abstractVM signature' runtimecode  storagemodel
     let args = drop 4 $ fst $ view (state . calldata) preStateRaw
     -- add the pre condition to the pathconditions to ensure that we are only exploring valid paths
     let preState = over pathConditions ((++) [pre args]) preStateRaw
-    smtState <- queryState
-    results <- io $ fst <$> runStateT (interpret (Fetch.oracle smtState Nothing) maxIter Stepper.runFully) preState
-    case (maybepost, results) of
-      (Just post, Right res) -> do let postC = sOr $ fmap (\postState -> (sAnd (view pathConditions postState)) .&& sNot (post (preState, postState))) res
-                                   -- is there any path which can possibly violate
-                                   -- the postcondition?
-                                   resetAssertions
-                                   constrain postC
-                                   io $ putStrLn "checking postcondition..."
-                                   sat <- checkSat
-                                   case sat of
-                                     Unk -> do io $ putStrLn "postcondition query timed out"
-                                               return $ Left (preState, res)
-                                     Unsat -> do io $ putStrLn "Q.E.D"
-                                                 return $ Left (preState, res)
-                                     Sat -> do io $ putStrLn "post condition violated:"
-                                               let (calldata', cdlen) = view (state . calldata) preState
-                                               cdlen <- num <$> getValue cdlen
+    verify preState maxIter maybepost
 
-                                               model <- mapM (getValue.fromSized) (take cdlen calldata')
-                                               return $ Right (pack model)
-      (Nothing, Right res) -> do io $ print "Q.E.D"
-                                 return $ Left (preState, res)
-      (_, Left _) -> error "unexpected error during symbolic execution"
+verify :: VM -> Maybe Integer -> Maybe Postcondition -> Query (Either (VM, [VM]) ByteString)
+verify preState maxIter maybepost = do
+  smtState <- queryState
+  results <- io $ fst <$> runStateT (interpret (Fetch.oracle smtState Nothing) maxIter Stepper.runFully) preState
+  case (maybepost, results) of
+    (Just post, Right res) -> do
+      let postC = sOr $ fmap (\postState -> (sAnd (view pathConditions postState)) .&& sNot (post (preState, postState))) res
+                  -- is there any path which can possibly violate
+                  -- the postcondition?
+      resetAssertions
+      constrain postC
+      io $ putStrLn "checking postcondition..."
+      sat <- checkSat
+      case sat of
+        Unk -> do io $ putStrLn "postcondition query timed out"
+                  return $ Left (preState, res)
+        Unsat -> do io $ putStrLn "Q.E.D"
+                    return $ Left (preState, res)
+        Sat -> do io $ putStrLn "post condition violated:"
+                  let (calldata', cdlen) = view (state . calldata) preState
+                  cdlen <- num <$> getValue cdlen
+                  model <- mapM (getValue.fromSized) (take cdlen calldata')
+                  return $ Right (pack model)
+
+    (Nothing, Right res) -> do io $ print "Q.E.D"
+                               return $ Left (preState, res)
+
+    (_, Left _) -> error "unexpected error during symbolic execution"
+
+equivalenceCheck :: ByteString -> ByteString -> Maybe Integer -> Maybe Text -> Query (Either ([VM], [VM]) ByteString)
+equivalenceCheck bytecodeA bytecodeB maxiter signature' = do
+  preStateA <- abstractVM signature' bytecodeA SymbolicS
+           
+  let preself = preStateA ^. state . contract
+      precaller = preStateA ^. state . caller
+      prestorage = preStateA ^?! env . contracts . ix preself . storage
+      (calldata', cdlen) = view (state . calldata) preStateA
+      preStateB = loadSymVM bytecodeB prestorage precaller (calldata', cdlen)
+
+  smtState <- queryState
+  (aRes, bRes) <- both (\x -> io $ fst <$> runStateT (interpret (Fetch.oracle smtState Nothing) maxiter Stepper.runFully) x)
+    (preStateA, preStateB)
+  case (aRes, bRes) of
+    (Left errA, Right _) -> error $ "A Failed: " <> show errA
+    (Right _, Left errB) -> error $ "B Failed: " <> show errB
+    (Left errA, Left errB) -> error $ "A Failed: " <> show errA <> "\nand B Failed:" <> show errB
+    (Right aVMs, Right bVMs) -> do
+      let differingEndStates = zipWith distinct aVMs bVMs
+          distinct a b = let (aPath, bPath) = both' (view pathConditions) (a, b)
+                             (aSelf, bSelf) = both' (view (state . contract)) (a, b)
+                             (aEnv, bEnv) = both' (view (env . contracts)) (a, b)
+                             (aResult, bResult) = both' (view result) (a, b)
+                             (Symbolic aStorage, Symbolic bStorage) = (view storage (aEnv ^?! ix aSelf), view storage (bEnv ^?! ix bSelf))
+                             differingResults = case (aResult, bResult) of
+                               (Just (VMSuccess aOut), Just (VMSuccess bOut)) -> aOut ./= bOut .|| aStorage ./= bStorage .|| fromBool (aSelf /= bSelf)
+                               (Just (VMFailure _), Just (VMFailure _)) -> sFalse
+                               (Just _, Just _) -> sTrue
+                               _ -> error "Internal error during symbolic execution (should not be possible)"
+                         in sAnd aPath .&& sAnd bPath .&& differingResults
+      -- If there exists a pair of endstates where this is not the case,
+      -- the following constraint is satisfiable
+      resetAssertions
+      constrain $ sOr differingEndStates
+
+      checkSat >>= \case
+         Unk -> error "solver said unknown!"
+         Sat -> do cdlen <- num <$> getValue cdlen
+                   model <- mapM (getValue.fromSized) (take cdlen calldata')
+                   return (Right (pack model))
+         Unsat -> return $ Left (aVMs, bVMs)
+
+
+-- prelude plz
+both' :: (a -> b) -> (a, a) -> (b, b)
+both' f (x, y) = (f x, f y)
