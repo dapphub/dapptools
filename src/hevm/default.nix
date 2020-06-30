62b15f227f9677cdad04a8f642466cbb38729a9f
Martin Lundfall <martin.lundfall@protonmail.com>
2020-06-30 14:05:18 +0200

    bump sbv to 8.6

--- src/hevm/default.nix
+++ src/hevm/default.nix
@@ -5,7 +5,7 @@
 , HUnit, lens, lens-aeson, megaparsec, memory, monad-par, mtl
 , multiset, operational, optparse-generic, process, QuickCheck
 , quickcheck-text, regex-tdfa, restless-git, rosezipper, s-cargot
-, sbv_8_4, scientific, secp256k1, semver-range, stdenv, tasty
+, sbv_8_6, scientific, secp256k1, semver-range, stdenv, tasty
 , tasty-hunit, tasty-quickcheck, temporary, text, text-format, time
 , transformers, tree-view, unordered-containers, vector, vty
 , witherable, wreq
@@ -23,7 +23,7 @@ mkDerivation {
     cryptonite data-dword deepseq directory fgl filepath free
     ghci-pretty haskeline lens lens-aeson megaparsec memory monad-par
     mtl multiset operational optparse-generic process QuickCheck
-    quickcheck-text restless-git rosezipper s-cargot sbv_8_4 scientific
+    quickcheck-text restless-git rosezipper s-cargot sbv_8_6 scientific
     semver-range temporary text text-format time transformers tree-view
     unordered-containers vector vty witherable wreq
   ];
@@ -32,12 +32,12 @@ mkDerivation {
     aeson ansi-wl-pprint async base base16-bytestring base64-bytestring
     binary brick bytestring containers cryptonite data-dword deepseq
     directory filepath free ghci-pretty lens lens-aeson memory mtl
-    optparse-generic process QuickCheck quickcheck-text regex-tdfa sbv_8_4
+    optparse-generic process QuickCheck quickcheck-text regex-tdfa sbv_8_6
     temporary text text-format unordered-containers vector vty
   ];
   testHaskellDepends = [
     base base16-bytestring binary bytestring free ghci-pretty here
-    HUnit lens mtl QuickCheck sbv_8_4 tasty tasty-hunit tasty-quickcheck
+    HUnit lens mtl QuickCheck sbv_8_6 tasty tasty-hunit tasty-quickcheck
     text vector
   ];
   testSystemDepends = [ secp256k1 ];
