ad2f8408b8bd2845b90cea44ecdabba1609dc4b4
Martin Lundfall <martin.lundfall@protonmail.com>
2020-07-13 17:17:11 +0200

    rebased to master

--- src/hevm/src/EVM/UnitTest.hs
+++ src/hevm/src/EVM/UnitTest.hs
@@ -182,7 +182,7 @@ checkFailures UnitTestOptions { .. } method args bailed = do
     Stepper.evm popTrace
     Stepper.evm $ setupCall testParams addr "failed()" args
     Stepper.note "Checking whether assertions failed"
-    res <- Stepper.execFullyOrFail >>= Stepper.decode (AbiBoolType . forceLitBytes)
+    res <- Stepper.execFullyOrFail >>= (Stepper.decode AbiBoolType) . forceLitBytes
     let AbiBool failed = res
     -- Return true if the test was successful
     pure (shouldFail == failed)
