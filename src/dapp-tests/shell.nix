0a3bb05b5e42c5f16776df19ac7c12a5db9f5112
Lorenzo Manacorda <lorenzo@mailbox.org>
2020-07-15 21:30:29 +0200

    no shellHook

--- src/dapp-tests/shell.nix
+++ src/dapp-tests/shell.nix
@@ -5,9 +5,5 @@
 pkgs.mkShell {
   name = "dapp-tests";
 
-  buildInputs = with pkgs; [ bash curl dapp seth solc ];
-
-  shellHook = ''
-    ./integration/tests.sh
-  '';
+  buildInputs = with pkgs; [ bash curl dapp gnumake seth solc ];
 }
