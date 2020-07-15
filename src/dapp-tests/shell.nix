a40a40816f3681b6c8bf2ac4028fe0365d934fba
Lorenzo Manacorda <lorenzo@mailbox.org>
2020-07-15 22:39:43 +0200

    hello hevm

--- src/dapp-tests/shell.nix
+++ src/dapp-tests/shell.nix
@@ -5,5 +5,5 @@
 pkgs.mkShell {
   name = "dapp-tests";
 
-  buildInputs = with pkgs; [ bash curl dapp gnumake seth solc ];
+  buildInputs = with pkgs; [ bash curl dapp gnumake hevm procps seth solc ];
 }
