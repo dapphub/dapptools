fc74da9e840b2bdafa98ed0c171b4f96b8abfdb7
Martin Lundfall <martin.lundfall@protonmail.com>
2020-07-24 15:58:52 +0200

    gomodules! what a journey

--- src/ethsign/default.nix
+++ src/ethsign/default.nix
@@ -1,45 +1,13 @@
-{ stdenv, buildGoPackage, fetchFromGitHub, fetchgit, go-ethereum, clang }:
+{ stdenv, buildGoModule, fetchFromGitHub, go-ethereum, fetchgit }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "ethsign-${version}";
-  version = "0.15.0";
+  version = "0.16.0";
 
-  goPackagePath = "github.com/dapphub/ethsign";
-  hardeningDisable = ["fortify"];
   src = ./.;
 
-  extraSrcs = [
-    {
-      goPackagePath = "github.com/ethereum/go-ethereum";
-      src = go-ethereum.src;
-    }
-    {
-      goPackagePath = "gopkg.in/urfave/cli.v1";
-      src = fetchFromGitHub {
-        owner = "urfave";
-        repo = "cli";
-        rev = "v1.19.1";
-        sha256 = "1ny63c7bfwfrsp7vfkvb4i0xhq4v7yxqnwxa52y4xlfxs4r6v6fg";
-      };
-    }
-    {
-      goPackagePath = "golang.org/x/crypto";
-      src = fetchgit {
-        url = "https://go.googlesource.com/crypto";
-        rev = "94eea52f7b742c7cbe0b03b22f0c4c8631ece122";
-        sha256 = "095zyvjb0m2pz382500miqadhk7w3nis8z3j941z8cq4rdafijvi";
-      };
-    }
-    {
-      goPackagePath = "golang.org/x/sys";
-      src = fetchgit {
-        url = "https://go.googlesource.com/sys";
-        rev = "53aa286056ef226755cd898109dbcdaba8ac0b81";
-        sha256 = "1yd17ccklby099cpdcsgx6lf0lj968hsnppp16mwh9009ldf72r1";
-      };
-    }
-  ];
-
+  modSha256 = "099hf9518zqgsjv050qlxj3la9f66j8bbbafh8vgivrw5vd81m60";
+  
   meta = with stdenv.lib; {
     homepage = http://github.com/dapphub/dapptools;
     description = "Make raw signed Ethereum transactions";
