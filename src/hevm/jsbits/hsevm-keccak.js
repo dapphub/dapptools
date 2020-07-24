30e4cc1bdf985e4ae33a4be92c2ea508453e3db7
Martin Lundfall <martin.lundfall@protonmail.com>
2020-07-24 11:52:07 +0200

    remove hevm js parts

--- src/hevm/jsbits/hsevm-keccak.js
+++
@@ -1,3 +0,0 @@
-function keccakBase64(x) {
-  return base64js.fromByteArray(keccak_256.array(base64js.toByteArray(x)))
-}
--- src/hevm/src/EVM/Keccak.hs
+++ src/hevm/src/EVM/Keccak.hs
@@ -1,9 +1,3 @@
-{-# Language CPP #-}
-
-#ifdef __GHCJS__
-{-# Language JavaScriptFFI #-}
-#endif
-
 module EVM.Keccak (keccak, abiKeccak) where
 
 import EVM.Types
@@ -16,39 +10,15 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.Word
 
-#ifdef __GHCJS__
-import qualified Data.JSString as JS
-import qualified Data.Text.Encoding as Text
-import qualified Data.Text as Text
-import qualified Data.ByteString.Base64 as BS64
-
-foreign import javascript unsafe
-  "keccakBase64($1)"
-  keccakBase64 :: JS.JSString -> JS.JSString
-
-keccakBytes =
-  BS64.encode
-    >>> Text.decodeUtf8
-    >>> Text.unpack
-    >>> JS.pack
-    >>> keccakBase64
-    >>> JS.unpack
-    >>> Text.pack
-    >>> Text.encodeUtf8
-    >>> BS64.decodeLenient
-
-#else
 import Crypto.Hash
 import qualified Data.ByteArray as BA
 
+keccakBytes :: ByteString -> ByteString
 keccakBytes =
   (hash :: ByteString -> Digest Keccak_256)
     >>> BA.unpack
     >>> BS.pack
 
-#endif
-
-keccakBytes :: ByteString -> ByteString
 
 word32 :: [Word8] -> Word32
 word32 xs = sum [ fromIntegral x `shiftL` (8*n)
