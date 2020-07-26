1847372909b98d432be148d5764e77b4429cb6d7
Martin Lundfall <martin.lundfall@protonmail.com>
2020-07-26 15:06:23 +0200

    nix-infra: use callCabal2nix instead of cabal2nix generated file

--- src/hevm/shell.nix
+++ src/hevm/shell.nix
@@ -2,25 +2,19 @@
 let
   inherit (dapphub) pkgs;
 
-  haskellPackages = pkgs.haskellPackages.extend (
-    self: super: {
-      ghc =
-        super.ghc // { withPackages = super.ghc.withHoogle; };
-      ghcWithPackages =
-        self.ghc.withPackages;
-    }
-  );
 
-  drv = pkgs.haskell.lib.addBuildTool (
-    haskellPackages.callPackage (import ./default.nix) {
-      inherit (pkgs) secp256k1;
-      ff = pkgs.libff;
-    }
-  ) [pkgs.git pkgs.cabal-install pkgs.cabal2nix pkgs.solc];
-
+  drv = pkgs.haskellPackages.shellFor {
+    packages = p: [
+      p.hevm
+    ];
+    buildInputs = with pkgs.haskellPackages; [
+      cabal-install
+    ];
+    withHoogle = true;
+  };
 in
   if pkgs.lib.inNixShell
-  then drv.env.overrideAttrs (_: {
+  then drv.overrideAttrs (_: {
     LD_LIBRARY_PATH = "${pkgs.secp256k1}/lib:${pkgs.libff}/lib";
   })
   else drv
