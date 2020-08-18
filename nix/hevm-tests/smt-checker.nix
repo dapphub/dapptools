fed682001c5f98ca50bd53c9822e8ece25448270
David Terry <me@xwvvvvwx.com>
2020-08-18 06:56:33 +0200

    nix: hevm-tests: smtChecker: bump timeout to 90s

--- nix/hevm-tests/smt-checker.nix
+++ nix/hevm-tests/smt-checker.nix
@@ -240,7 +240,7 @@ let
 
     explore() {
       set -x
-      hevm_output=$(${timeout} 60s ${hevm} symbolic --code "$1" --solver "$2" --json-file "$3" $4 2>&1)
+      hevm_output=$(${timeout} 90s ${hevm} symbolic --code "$1" --solver "$2" --json-file "$3" $4 2>&1)
       status=$?
       set +x
 
