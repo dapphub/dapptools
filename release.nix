7243fa3923545d244223bf1b1b09b56462f5495e
Martin Lundfall <martin.lundfall@protonmail.com>
2020-10-18 15:35:30 +0200

    skip hevm symbolic tests

--- release.nix
+++ release.nix
@@ -45,7 +45,7 @@ let
     hevm-compliance = hevmCompliance dist;
   # the union is necessary because nix-build does not evaluate sets
   # recursively, and `solc-versions` and `hevm-tests` are sets.
-  } // dist.pkgs.solc-versions // dist.pkgs.hevm-tests ;
+  } // dist.pkgs.solc-versions ; # // dist.pkgs.hevm-tests ;
 
 in {
   dapphub.linux.stable = stable linux;
