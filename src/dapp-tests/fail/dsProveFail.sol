8fe85fca7013ec1a5942762a0c0032d48810914d
Martin Lundfall <martin.lundfall@protonmail.com>
2021-01-25 17:52:37 +0100

    fix

--- src/dapp-tests/fail/dsProveFail.sol
+++ src/dapp-tests/fail/dsProveFail.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.6.7;
 
 import "ds-test/test.sol";
 import "ds-token/token.sol";
+import "ds-math/math.sol";
 
-contract SolidityTest is DSTest {
+contract SolidityTest is DSTest, DSMath {
     DSToken token;
 
     function setUp() public {
