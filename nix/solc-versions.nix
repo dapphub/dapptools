5c566e34d9451e035514f376b99a2a4afe345f2a
Martin Lundfall <martin.lundfall@protonmail.com>
2021-02-02 13:59:39 +0100

    add solc:0.8.1 to solc-versions

--- nix/solc-versions.nix
+++ nix/solc-versions.nix
@@ -47,7 +47,8 @@ rec {
     solc_0_6_7  = { rev = "b777e1388590b62caa6b31c9258c3ac75e31675e"; sha256 = "0qk0ggl8ac5m2wmcqva9ryvn3fmjj85333mnkb6688fksqmdbphs"; };
     solc_0_6_8  = { rev = "9d68154c4128d6abb435083c527f4b4570a0c4cb"; sha256 = "0pm3x1nwxlbk04hhp15azr2c34rhawalji8g730p1i01i9icq3lz"; };
     solc_0_6_12  ={ rev = "d8136afba5e0442b5bd5f4c83a670e6696f6f350"; sha256 = "01v6vzq57c5l8i79cmhxvf50hi9r75mbqggd0wand9sjzz9q72fi"; };
+    solc_0_8_1   ={ rev = "f369740ed3956dfcc1d39fdc1058dd12f35c26c1"; sha256 = "1laka3y0mqja4719fgi1hj19s1176a60hkr147p82g"; };
   };
 
-  unreleased_x86_64-darwin = removeAttrs unreleased_x86_64-linux [ "solc_0_4_18" "solc_0_6_12" ];
+  unreleased_x86_64-darwin = removeAttrs unreleased_x86_64-linux [ "solc_0_4_18" "solc_0_6_12" "solc_0_8_1" ];
 }
