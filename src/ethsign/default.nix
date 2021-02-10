8746b1962aa243bca387335241f87e114670bf7b
David Terry <me@xwvvvvwx.com>
2021-02-10 10:08:08 +0100

    nix: bump nixpkgs

--- src/ethsign/default.nix
+++ src/ethsign/default.nix
@@ -6,8 +6,8 @@ buildGoModule rec {
 
   src = ./.;
 
-  modSha256 = "099hf9518zqgsjv050qlxj3la9f66j8bbbafh8vgivrw5vd81m60";
-  
+  vendorSha256 = "099hf9518zqgsjv050qlxj3la9f66j8bbbafh8vgivrw5vd81m60";
+
   meta = with stdenv.lib; {
     homepage = http://github.com/dapphub/dapptools;
     description = "Make raw signed Ethereum transactions";
