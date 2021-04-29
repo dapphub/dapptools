71ae4886a47c5c80704144bad00d34d581177140
Martin Lundfall <martin.lundfall@protonmail.com>
2021-04-29 23:32:32 +0200

    install z3 and cvc4 in ci too

---
+++ .github/scripts/install-cvc4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Adapted from https://github.com/commercialhaskell/stack
+
+set -eux
+
+mkdir -p $HOME/.local/bin;
+
+travis_retry() {
+  cmd=$*
+  $cmd || (sleep 2 && $cmd) || (sleep 10 && $cmd)
+}
+
+fetch_cvc4_linux() {
+  VER="$1"
+  wget "https://github.com/CVC4/CVC4/releases/download/$VER/cvc4-$VER-x86_64-linux-opt"
+  chmod +x "cvc4-$VER-x86_64-linux-opt"
+  mv "cvc4-$VER-x86_64-linux-opt" "$HOME/.local/bin/cvc4"
+  echo "Downloaded cvc4 $VER"
+}
+
+if [ "$HOST_OS" = "Linux" ]; then
+    travis_retry fetch_cvc4_linux "1.8"
+fi
