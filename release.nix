5b07ae34ddb741c90577fdc904a4304773dfb690
Martin Lundfall <martin.lundfall@protonmail.com>
2021-05-11 17:11:31 +0200

    don't build hevmUnwrapped in release

--- release.nix
+++ release.nix
@@ -31,7 +31,6 @@ let
   # These packages should always work and be available in the binary cache.
   stable = dist: with dist.pkgs; {
     inherit hevm;
-    inherit hevmUnwrapped;
     inherit dapp;
     inherit ethsign;
     inherit go-ethereum-unlimited;
