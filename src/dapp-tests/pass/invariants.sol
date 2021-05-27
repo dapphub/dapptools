b47a343ae0e3a38f21f79cdadce361525a7654c2
Martin Lundfall <martin.lundfall@protonmail.com>
2021-05-27 16:24:49 +0200

    some basic tests for invariant tests

---
+++ src/dapp-tests/pass/invariants.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.6.7;
+
+import "ds-test/test.sol";
+import "ds-token/token.sol";
+import "ds-math/math.sol";
+
+contract InvariantTest is DSTest {
+    DSToken token;
+
+    function setUp() public {
+        token = new DSToken("TKN");
+        token.mint(100 ether);
+    }
+
+    function invariantTestThisBal() {
+        assertLe(token.balanceOf(address(this)), 100 ether);
+    }
+    function invariantTotSupply() {
+        assertEq(token.totalSupply(), 100 ether);
+    }
+}
