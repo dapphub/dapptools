e63043a25d520d55d72cdcd34b53a6d83b3f0a73
Artur Cygan <arczicygan@gmail.com>
2021-06-06 00:00:15 +0200

    Update channel

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
 { system ? builtins.currentSystem , ... }:
 
 let
-  rev = "d25ea6a0d2a847fb52131da546f2a866656fbafa";
+  rev = "aa576357673d609e618d87db43210e49d4bb1789";
   nixpkgs = builtins.fetchTarball {
     name = "nixpkgs-release-21.05";
     url = "https://github.com/nixos/nixpkgs/tarball/${rev}";
-    sha256 = "0y2h71pvzrhc2svib4lwjl04hrzy8901ravvlyxlqdbal8hy3838";
+    sha256 = "1868s3mp0lwg1jpxsgmgijzddr90bjkncf6k6zhdjqihf0i1n2np";
   };
 in
   # Now return the Nixpkgs configured to use our overlay.
