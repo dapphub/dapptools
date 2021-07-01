c729a52ad32148bb0f9c129dd194633d8a6645c7
David Terry <me@xwvvvvwx.com>
2021-07-01 12:28:57 +0200

    nix: add shared haskell package set

--- haskell.nix
+++ haskell.nix
@@ -2,7 +2,7 @@
 # to our Haskell package extensions from other overlays, bypassing the
 # rest of our overlay.  This was necessary for rather obscure reasons.
 
-{ pkgs, lib, wrapped ? true }:
+{ pkgs, lib, wrapped ? true, shared ? false }:
 
 let
   stdenv = pkgs.stdenv;
@@ -57,7 +57,8 @@ in self-hs: super-hs:
       configureFlags = attrs.configureFlags ++ [
           "--ghc-option=-O2"
       ] ++
-      (if stdenv.isDarwin then [] else [
+      (if stdenv.isDarwin then [] else
+        if shared then [] else [
           "--enable-executable-static"
           "--extra-lib-dirs=${pkgs.gmp.override { withStatic = true; }}/lib"
           "--extra-lib-dirs=${pkgs.glibc.static}/lib"
