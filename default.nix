5480ee91d3446bf963544cab62b34b3842faed18
Martin Lundfall <martin.lundfall@protonmail.com>
2021-07-08 14:52:45 +0200

    update nixpkgs

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
 { system ? builtins.currentSystem , ... }:
 
 let
-  rev = "aa576357673d609e618d87db43210e49d4bb1789";
+  rev = "3d64b6067f1c457f889e91c59ced273be1d1e4f1";
   nixpkgs = builtins.fetchTarball {
     name = "nixpkgs-release-21.05";
     url = "https://github.com/nixos/nixpkgs/tarball/${rev}";
-    sha256 = "1868s3mp0lwg1jpxsgmgijzddr90bjkncf6k6zhdjqihf0i1n2np";
+    sha256 = "0wgmzq2h82wxqh6zkxxxw77rdk75m48rshmjfl3w35pv498wfcwz";
   };
 in
   # Now return the Nixpkgs configured to use our overlay.
@@ -14,4 +14,3 @@ in
 
     overlays = [(import ./overlay.nix)];
   }
-
