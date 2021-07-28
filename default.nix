bb70d12d678a787a91ca847b35ea757bf3e792e0
Martin Lundfall <martin.lundfall@protonmail.com>
2021-07-28 17:22:09 +0200

    use normal nixpkgs and custom geth

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
 { system ? builtins.currentSystem , ... }:
 
 let
-  rev = "3d64b6067f1c457f889e91c59ced273be1d1e4f1";
+  rev = "aa576357673d609e618d87db43210e49d4bb1789";
   nixpkgs = builtins.fetchTarball {
     name = "nixpkgs-release-21.05";
     url = "https://github.com/nixos/nixpkgs/tarball/${rev}";
-    sha256 = "0wgmzq2h82wxqh6zkxxxw77rdk75m48rshmjfl3w35pv498wfcwz";
+    sha256 = "1868s3mp0lwg1jpxsgmgijzddr90bjkncf6k6zhdjqihf0i1n2np";
   };
 in
   # Now return the Nixpkgs configured to use our overlay.
