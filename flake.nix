2f053490fb8660eb0639ef125370f77b4929ac93
Ian Macalinao <ian@moving.wtf>
2022-09-05 11:48:00 -0500

    Adds Nix flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''
+    A suite of Ethereum focused CLI tools following the Unix design philosophy,
+    favoring composability, configurability and extensibility.
+  '';
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=aa576357673d609e618d87db43210e49d4bb1789";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      systems =
+        flake-utils.lib.eachDefaultSystem (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ (import ./overlay.nix) ];
+            };
+          in
+          {
+            packages = pkgs.solc-static-versions // {
+              inherit (pkgs) solc hevm hevmUnwrapped jays jshon
+                seth dapp ethsign token go-ethereum-unlimited
+                qrtx qrtx-term secp256k1;
+            };
+          }
+        );
+    in
+    systems // {
+      overlays = {
+        default = import ./overlay.nix;
+      };
+    };
+}
