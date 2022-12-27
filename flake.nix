05d1d4ecc276b3b92c79ca05c8f96e7a6822d558
Lorenzo Manacorda <lorenzo@mailbox.org>
2022-12-27 16:09:03 +0100

    flake: use this repo's hevm
    Had to remove aarch64-darwin, as GHC 8.10.4 isn't supported on this
    platform.

--- flake.nix
+++ flake.nix
@@ -4,18 +4,18 @@
   inputs = {
     # same as in default.nix
     nixpkgs.url = "github:NixOS/nixpkgs/aa576357673d609e618d87db43210e49d4bb1789";
-    ethereum-hevm.url = "github:ethereum/hevm";
   };
 
   nixConfig = {
+    # required to build hevm
+    allow-import-from-derivation = true;
     extra-substituters = [ "https://dapp.cachix.org" ];
     extra-substituters-public-keys = [ "dapp.cachix.org-1:9GJt9Ja8IQwR7YW/aF0QvCa6OmjGmsKoZIist0dG+Rs=" ];
   };
 
-  outputs = { self, nixpkgs, ethereum-hevm }:
+  outputs = { self, nixpkgs }:
     let
       supportedSystems = [
-        "aarch64-darwin"
         "aarch64-linux"
 
         "x86_64-darwin"
@@ -23,7 +23,10 @@
       ];
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ (import ./overlay.nix) ];
+      });
     in
     {
       packages =
@@ -36,7 +39,7 @@
           rec {
             dapp = pkgs.callPackage (import ./src/dapp) { inherit dapptoolsSrc hevm seth; };
             ethsign = pkgs.callPackage (import ./src/ethsign) { };
-            hevm = ethereum-hevm.packages.${system}.hevm;
+            hevm = pkgs.hevm;
             seth = pkgs.callPackage (import ./src/seth) { inherit dapptoolsSrc hevm ethsign; };
           });
 
