85b9dc66ca4e67bf0c175c1e60c896d0ec65d511
Lorenzo Manacorda <lorenzo@mailbox.org>
2023-01-14 11:05:21 +0100

    release: remove ethereum-test-suite
    Unused

--- release.nix
+++ release.nix
@@ -5,13 +5,6 @@ let
   linux = system "x86_64-linux";
   darwin = system "x86_64-darwin";
 
-  ethereum-test-suite = x: x.fetchFromGitHub {
-    owner = "ethereum";
-    repo = "tests";
-    rev = "e20d7f39aae1e33394ae6b94590d15083e224fa5";
-    sha256 = "1i68k3b8sxawbm65mwph8d5ld9jdjh08c6hln0vygjgwmd0j4n30";
-  };
-
   # These packages should always work and be available in the binary cache.
   stable = dist: with dist.pkgs; {
     inherit dapp;
